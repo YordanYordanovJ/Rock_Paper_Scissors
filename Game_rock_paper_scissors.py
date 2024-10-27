8cecb78b0ad0a129bef5060b6e846b432dff96dc
Yordan Yordanov <119543781+YordanYordanovJ@users.noreply.github.com>
2024-10-27 22:35:44 +0200

    Add files via upload
    Full Rock , Paper , Scissors game in python ! :)

---
+++ Game_rock_paper_scissors.py
@@ -0,0 +1,33 @@
+import random
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+
+player_choise = input("Choose [r]ock, [p]aper or [s]cissors: ")
+
+if player_choise == "r":
+    player_choise = rock
+elif player_choise == "p":
+    player_choise = paper
+elif player_choise == "s":
+    player_choise = scissors
+else:
+    raise SystemExit("Invalid Input. Try again...")
+
+computer_random_number = random.randint(1, 3)
+if computer_random_number == 1 :
+    computer_random_number = rock
+elif computer_random_number == 2:
+    computer_random_number = paper
+else:
+    computer_random_number = scissors
+print(f"The computer chose {computer_random_number}.")
+
+if player_choise == rock and computer_random_number == scissors or\
+        player_choise == paper and computer_random_number == rock or\
+        player_choise == scissors and computer_random_number == paper:
+    print("You win!")
+elif player_choise == computer_random_number :
+    print("Draw!")
+else:
+    print("You lose :(")
